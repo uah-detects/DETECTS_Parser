13bf6eac878f69afe18d5085f4cf180d431bcc81
uah-detects <93224539+uah-detects@users.noreply.github.com>
2022-02-25 17:08:16 -0600

    Started Regex

--- scripts/js/detects_parser.js
+++ scripts/js/detects_parser.js
@@ -1,3 +1,9 @@
+var headerDataArray = [];
+var bodyDataArray = [];
+var rawFileVerificationHeaderArray = ["time","lasttime","lat","lng","speed","course","altitude","comment"];
+
+
+
 function readDataFile(e) {
 
     var file = e.target.files[0];
@@ -7,10 +13,104 @@ function readDataFile(e) {
     var reader = new FileReader();
     reader.onload = function(e) {
         var fileContentArray = this.result.split(/\r\n|\n/);
-      console.log(fileContentArray);
-      
-    };
-    reader.readAsText(file);
+
+        console.log(fileContentArray);
+
+        var headerLine = splitHeader(fileContentArray);
+
+        if (verificationHeaderCheck(headerLine) == true)
+        {
+            console.log("in read --> " + headerLine);
+
+            var headerSize = headerLine.length;
+            console.log(headerSize);
+
+            //parsing the body of the data
+            var bodyArray = splitBody(headerSize, fileContentArray);
+            console.log(bodyArray);
+
+            //console.log(headerLine);
+
+            console.log(bodyArray);
+
+            //Setting global arrays
+            headerDataArray = headerLine;
+            bodyDataArray = bodyArray;
+
+            // regex Expression for comment section: /" StrTrk\s\d\d\s\d\s\d\.\d\dV\s\d\dC\s\d\d\d\d\dPa "/i
+
+            /*for(let j = 0; j < bodyArray[0].length; j++)            //First loop is looping through the file line by line
+            {
+
+            }*/
+            console.log(useRegex(" StrTrk 35 9 1.63V 27C 97893Pa "));
+        }
+
+        
+      };
+      reader.readAsText(file);
+}
+
+window.onload = function ()
+{
+    console.log(useRegex("StrTrk 35 9 1.63V 27C 97893Pa"));
+};
+
+function useRegex(input) {
+    let regex = /StrTrk\s[0-9]{1-4}\s[0-9]\s[0-9]\.[0-9]{1-2}V\s[0-9]{1-2}C\s[0-9]{1-6}Pa/i;
+    return regex.test(input);
+}
+
+function verificationHeaderCheck(headerLine)
+{
+  var headerLineArray = [];
+  headerLineArray = headerLine;
+  console.log( headerLineArray);
+  console.log(rawFileVerificationHeaderArray);
+  if(headerLine.length == rawFileVerificationHeaderArray.length)
+  {
+    for(let i = 0; i <rawFileVerificationHeaderArray.length; i++)
+    {
+      if(rawFileVerificationHeaderArray[i] != headerLine[i])
+      {
+        return false;
+      }
+    }
+    return true;
   }
+  return false;
+
+}
+
+function splitHeader(fileContentArray)
+{
+  var headerLine = fileContentArray[0].split(',');
+  console.log(headerLine);
+  return headerLine;
+}
+function splitBody(headerSize,fileContentArray)
+{
+  var dataArray = [];
+  var headerLine = fileContentArray[0].split(',');
+
+  for(let i = 0; i < headerSize; i++)                         //Creating each array column of the multidimensional Array
+  {
+      dataArray.push([]);
+  }
+  console.log(dataArray);
+  
+  for(let j = 1; j < fileContentArray.length - 1; j++)            //First loop is looping through the file line by line Note: loop starts at one to leave off the header title that is stored in position 0 of the subarray
+  {
+    var parseLine = fileContentArray[j].split(',');
+
+    for(let k = 0; k < headerSize; k++)                       //Looping through each line item, item by item
+    {
+      dataArray[k].push(parseLine[k]);
+    }
+
+  }
+
+  return dataArray;
+}
 
   document.getElementById('fileinput').addEventListener('change', readDataFile, false);  // Listener for the Data File input
